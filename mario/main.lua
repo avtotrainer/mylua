d0f4e28079ce0a1c943d679e1c6c8632020f2dfc
avtotrainer <trainer@din.ge>
2024-07-10 09:35:06 +0400

    ბეგრაუნდი- არაა სპრაიტის ფაილები

--- mario/main.lua
+++ mario/main.lua
@@ -4,12 +4,41 @@ local Mario = require("mario")
 
 -- მენიუს მდგომარეობა
 local menuActive = false
+-- Mario-ს ობიეთქტი
 local mario
 
+-- გზის ტაილები (ნახატი)
+local groundTiles = {}
+--
 -- love.load ფუნქცია გამოიყენება თამაშის დასაწყებად
 function love.load()
 	-- ვქმნით Mario-ს ობიექტს საწყისი პოზიციით (100, 100)
-	mario = Mario:create(100, 100)
+	mario = Mario:create(100, 100, {
+		walk = love.graphics.newImage("mario_walk.png"),
+		idle = love.graphics.newImage("mario_idle.png"),
+		jump = love.graphics.newImage("mario_jump.png"),
+	})
+
+	-- ვქმნით და ვტვირთავთ გზის ტაილებს
+	local tile = love.graphics.newCanvas(32, 32)
+	love.graphics.setCanvas(tile)
+	love.graphics.clear()
+	love.graphics.setColor(139 / 255, 69 / 255, 192 / 255) -- ყავისფერი
+	love.graphics.rectangle("fill", 0, 8, 32, 24)
+
+	love.graphics.setColor(34 / 255, 139 / 255, 34 / 255) -- მწვანე
+	love.graphics.rectangle("fill", 0, 0, 32, 8)
+
+	love.graphics.setColor(105 / 255, 58 / 255, 16 / 255)
+	love.graphics.rectangle("fill", 6, 20, 2, 2)
+	love.graphics.rectangle("fill", 14, 14, 2, 2)
+	love.graphics.rectangle("fill", 14, 20, 2, 2)
+	love.graphics.rectangle("fill", 24, 18, 2, 2)
+
+	love.graphics.setCanvas()
+	for i = 1, 10 do
+		table.insert(groundTiles, { image = tile, x = i * 32, y = love.graphics.getHeight() - 32 })
+	end
 end
 
 -- love.update ფუნქცია განახლდება ყოველ კადრზე
@@ -23,6 +52,10 @@ end
 -- love.draw ფუნქცია გამოიყენება გრაფიკის დასახატად
 function love.draw()
 	if not menuActive then
+		-- ვხატავთ გზის ტაილებს
+		for _, tile in ipairs(groundTiles) do
+			love.graphics.draw(tile.image, tile.x, tile.y)
+		end
 		-- ვხატავთ Mario-ს ეკრანზე თუ მენიუ არ არის აქტიური
 		mario:draw()
 	else
--- mario/mario.lua
+++ mario/mario.lua
@@ -2,11 +2,12 @@
 -- Mario-ს პერსონაჟის მოდული
 
 -- ვქმნით ცარიელ ცრილს Mario-სთვის და ვაძლევთ მას ინდექსს
-Mario = {}
+
+local Mario = {}
 Mario.__index = Mario
 
 -- ფუნქცია Mario-ს შექმნისთვის
-function Mario:create(x, y)
+function Mario:create(x, y, imagePaths)
 	-- ვქმნით ცარიელ ობიექტს
 	local mario = {}
 	-- ვქმნით Mario-ს ობიექტს
@@ -14,9 +15,27 @@ function Mario:create(x, y)
 	-- ვაძლევთ Mario-ს საწყის პოზიციას
 	mario.x = x
 	mario.y = y
+	mario.images = {
+		love.graphics.newImage(imagePaths.walk),
+		love.graphics.newImage(imagePaths.idle),
+		love.graphics.newImage(imagePaths.jump),
+	}
+	mario.currentImage = mario.images.idle
+	mario.frame = 1
+	mario.timer = 0
+	mario.frameDuration = 0.1
+	mario.frames = {}
+	local image = mario.images.walk
+	local width = image:getWidth() / 3
+	local height = image:getHeight()
+	for i = 0, 2 do
+		table.insert(mario.frames, love.graphics.newQuad(i * Width, 0, width, height, image:getDimensions()))
+	end
+
+	-- mario.image = mario.currentImage
 	-- ვაძლევთ Mario-ს ზომებს
-	mario.width = 32
-	mario.height = 32
+	mario.width = width
+	mario.height = height
 	-- ვაძლევთ Mario-ს სიჩქარეს
 	mario.speed = 200
 	-- ვერტიკალური სიჩქარე
@@ -33,14 +52,33 @@ end
 
 -- ფუნქცია Mario-ს მოძრაობისთვის
 function Mario:move(dt)
+	local moving = false
 	-- თუ მარცხენა ღილაკი დაჭერილია
 	if love.keyboard.isDown("left") then
 		-- ვამცირებთ x კოორდინატს, რათა Mario მარცხნივ გადავიდეს
 		self.x = self.x - self.speed * dt
+		self.currentImage = self.images.walk
+		-- self.frame = 1
+		moving = true
 	-- თუ მარჯვენა ღილაკი დაჭერილია
 	elseif love.keyboard.isDown("right") then
 		-- ვზრდით x კოორდინატს, რათა Mario მარჯვნივ გადავიდეს
 		self.x = self.x + self.speed * dt
+		self.currentImage = self.images.walk
+		-- self.frame = 1
+		moving = true
+	else
+		self.currentImage = self.images.idle
+	end
+
+	if moving then
+		self.timer = self.timer + dt
+		if self.timer > self.frameDuration then
+			self.timer = self.timer - self.frameDuration
+			self.frame = self.frame % #self.frames + 1
+		end
+	else
+		self.frame = 1
 	end
 	-- ვერტიკალური მოძრაობა
 	if self.isJumping then
@@ -48,12 +86,13 @@ function Mario:move(dt)
 		self.dy = self.dy + self.gravity * dt
 		-- ვცვლით მარიოს ვერტიკალურ პოზიციას
 		self.y = self.y - self.dy * dt
-
+		self.currentImage = self.images.jump
 		-- მიწაზე დაბრუნების შემთხვევაში
 		if self.y > 100 then
 			self.y = 100
 			self.dy = 0
 			self.isJumping = false
+			self.currentImage = self.images.idle
 		end
 	end
 end
@@ -67,8 +106,11 @@ function Mario:jump()
 end
 --: ფუნქცია Mario-ს დასახატად
 function Mario:draw()
-	-- ვხატავთ Mario-ს როგორც შავს კვადრატს
-	love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+	if self.currentImage == self.images.walk then
+		love.graphics.draw(self.currentImage, self.frames[self.frame], self.x, self.y)
+	else
+		love.graphics.draw(self.currentImage, self.x, self.y)
+	end
 end
 
 -- ვაბრუნებთ Mario-ს მოდულს
